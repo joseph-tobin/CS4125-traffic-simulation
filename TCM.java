f6cab75d8817d9fd3117e2d2652a92f4acdecda0
Adam O Mahony <omahonyadam@gmail.com>
2019-10-24 13:17:57 +0100

    Migrate to Maven build tool

--- TCM.java
+++
@@ -1,9 +0,0 @@
-public class TCM extends Node{
-
-	public TCM(float x, float y){
-		super(x,y);
-	}
-
-	// needs more vars and methods
-
-}
---
+++ src/main/java/TCM.java
@@ -0,0 +1,15 @@
+public class TCM extends Node {
+
+	public TCM(float x, float y){
+		super(x,y);
+	}
+
+	// needs more vars and methods
+
+	/**
+	 * Abstract create method to be used in the TCM factory
+	 * @return
+	 */
+	private abstract TCM create();
+
+}
