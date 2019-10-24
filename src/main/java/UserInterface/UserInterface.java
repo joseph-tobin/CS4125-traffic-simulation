65aec15d8db6e9f118d48d63a40fa67a7dcac3de
nialldillane <niall.dillane@gmail.com>
2019-10-24 16:24:51 +0100

    Processing import

--- src/main/java/UserInterface/UserInterface.java
+++ src/main/java/UserInterface/UserInterface.java
@@ -1,37 +1,42 @@
 package UserInterface;
 
 import Sim.Simulation;
+import processing.core.PApplet;
 
-public class UserInterface{
+public class UserInterface extends PApplet {
 	// needs a rethink? depedency?
 	UIController uic;
 	Simulation sim;
 
-	public void draw(){
+	public static void main(String [] args){
+	    PApplet.main("MainClass", args);
+    }
 
-	}
-
-	public void save(){
-
-	}
-
-	public void load(){
-
-	}
-
-	public void run(){
-		sim.run();
-	}
-
-	public void pause(){
-		sim.pause();
-	}
-
-	public void reset(){
-		sim.reset();
-	}
-
-	public void displayMetrics(){
-		uic.getMetrics();
-	}
+//	public void draw(){
+//
+//	}
+//
+//	public void save(){
+//
+//	}
+//
+//	public void load(){
+//
+//	}
+//
+//	public void run(){
+//		sim.run();
+//	}
+//
+//	public void pause(){
+//		sim.pause();
+//	}
+//
+//	public void reset(){
+//		sim.reset();
+//	}
+//
+//	public void displayMetrics(){
+//		uic.getMetrics();
+//	}
 }
