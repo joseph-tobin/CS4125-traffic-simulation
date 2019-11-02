65ec0dd6dd437481bf80871d1392b9e259123de5
dairelavelle1 <16192249@studentmail.ul.ie>
2019-11-02 16:30:11 +0000

    fixed Vehicle

--- src/main/java/CS4125/Sim/Vehicle.java
+++ src/main/java/CS4125/Sim/Vehicle.java
@@ -14,35 +14,36 @@ public class Vehicle {
 		startNode=start;
 		currentNode=start;
 		endNode=end;
-		allNodes=new ArrayList<Node>(Arrays.asList(allNodes));
-		private ArrayList<Node> routeArray=new ArrayList<Node>();
+		ArrayList<Node> routeArray=new ArrayList<Node>();
 		routeArray.add(start);
 		routeFind(routeArray);
 	}
-	public Vehicle(Vehicle in){
-		this.currentNode=in.getCurrentNode();
-		this.lastNode=in.getLastNode();
-		this.startNode=in.getStartNode();
-		this.endNode=in.getEndNode();
-		this.route=in.routeFind();
+	public Vehicle(Node curr, Node last, Node start, Node end, ArrayList<Node> route){
+		this.currentNode=curr;
+		this.lastNode=last;
+		this.startNode=start;
+		this.endNode=end;
+		this.route=route;
 	}
-
+	public Vehicle copy(){
+		return new Vehicle(this.currentNode, this.lastNode, this.startNode, this.endNode, this.route);
+	}
 	public Node getCurrentNode()		{return currentNode;}
 	public Node getLastNode()			{return lastNode;}
 	public Node getStartNode()			{return startNode;}
 	public Node getEndNode()			{return endNode;}
-	public ArrayList<Node> routeFind()	{return route;}
+	public ArrayList<Node> getRoute()	{return route;}
 
 	private void routeFind(ArrayList<Node> in){
 		Node lastIn=in.get(in.size()-1);
-		if (lastIn=lastNode) {
+		if (lastIn==lastNode) {
 			route=in;
 		}
 		ArrayList<Node> temp=in;
-		if(in.getAdjacent().size()>1) {
-			for (int i=0; i<lastIn.getAdjacent.size(); i++) {
-				if (lastIn.getAdjacent.get(i)!=in.get(in.size()-2)) {
-					temp.add(lastIn.getAdjacent.get(i));
+		if(lastIn.getAdjacent().size()>1) {
+			for (int i=0; i<lastIn.getAdjacent().size(); i++) {
+				if (lastIn.getAdjacent().get(i)!=in.get(in.size()-2)) {
+					temp.add(lastIn.getAdjacent().get(i));
 					routeFind(temp);
 				}
 			}
@@ -50,7 +51,7 @@ public class Vehicle {
 	}
 
 	public void move() {
-		for (int i=0; i<route.size()-1; i++) {
+		for (int i=0; i<this.route.size()-1; i++) {
 			if (route.get(i)==currentNode) {
 				lastNode=currentNode;
 				currentNode=route.get(i+1);
