2f2996925a8e487d9bd0cb2423b26f90d60b47da
nialldillane <niall.dillane@gmail.com>
2019-11-03 15:45:43 +0000

    UI touches (soft)

--- src/main/java/CS4125/UserInterface/UserInterface.java
+++ src/main/java/CS4125/UserInterface/UserInterface.java
@@ -3,6 +3,8 @@ package CS4125.UserInterface;
 import javafx.animation.PathTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.*;
 import javafx.scene.paint.Color;
@@ -12,13 +14,14 @@ import javafx.util.Duration;
 
 public class UserInterface extends Application {
 
-	Pane pane = new Pane();
-	Scene scene = new Scene(pane, 600, 600);
+	SplitPane split = new SplitPane();
+	Pane pane = new Pane(new Label("Sim"));
+	Pane control = new Pane(new Label("Control"));
+	Scene scene;
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		stage.setScene(scene);
-		stage.show();
+		setup(stage);
 
 		Circle n1 = addNode(200, 200);
 		Circle n2 = addNode(300, 400);
@@ -28,6 +31,14 @@ public class UserInterface extends Application {
 		addCar(n1, n2);
 		addCar(n2, n1);
 
+	}
+
+	private void setup(Stage stage) {
+		split.setDividerPositions(0.75);
+		split.getItems().addAll(pane, control);
+		scene = new Scene(split, 900, 600);
+		stage.setScene(scene);
+		stage.show();
 	}
 
 	private Circle addNode(double x, double y) {
@@ -44,23 +55,25 @@ public class UserInterface extends Application {
 		edge.endXProperty().bind(n2.centerXProperty());
 		edge.endYProperty().bind(n2.centerYProperty());
 		pane.getChildren().add(edge);
+		edge.toBack();
 	}
 
 	private void addCar(Circle n1, Circle n2) {
-		Circle c1 = new Circle(n1.getCenterX(), n1.getCenterY(), 5);
-		c1.setFill(Color.INDIANRED);
-		pane.getChildren().add(c1);
+		Circle c = new Circle(n1.getCenterX(), n1.getCenterY(), 5);
+		c.setFill(Color.INDIANRED);
+		pane.getChildren().add(c);
+		c.toFront();
 
-		Path p1 = new Path();
-		p1.getElements().add(new MoveTo(n1.getCenterX(), n1.getCenterY()));
-		p1.getElements().add(new LineTo(n2.getCenterX(), n2.getCenterY()));
+		Path p = new Path();
+		p.getElements().add(new MoveTo(n1.getCenterX(), n1.getCenterY()));
+		p.getElements().add(new LineTo(n2.getCenterX(), n2.getCenterY()));
 
-		PathTransition t1 = new PathTransition();
-		t1.setNode(c1);
-		t1.setDuration(Duration.millis(1000));
-		t1.setCycleCount(PathTransition.INDEFINITE);
-		t1.setPath(p1);
-		t1.play();
+		PathTransition t = new PathTransition();
+		t.setNode(c);
+		t.setDuration(Duration.millis(1000));
+		t.setCycleCount(PathTransition.INDEFINITE);
+		t.setPath(p);
+		t.play();
 	}
 
 	public static void main(String[] args) {
