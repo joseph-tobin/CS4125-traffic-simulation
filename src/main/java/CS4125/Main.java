6f167ae8048f59f3c781b6ac84d8bddfae82b003
JoeTheToe <16163842@studentmail.ul.ie>
2019-11-11 17:27:57 +0000

    made simulation singleton

--- src/main/java/CS4125/Main.java
+++ src/main/java/CS4125/Main.java
@@ -5,9 +5,15 @@ import CS4125.Sim.Simulation;
 public class Main {
 
     public static void main(String[] args) {
-        Simulation sim = new Simulation();
-        sim.run();
+        //Simulation sim = new Simulation();
+        Simulation.INSTANCE.run();
+        //sim.run();
+        Simulation.INSTANCE.getVehicles().get(0).move();
 
-        System.out.println(sim.getNodeList().get(0).getX());
+        System.out.println(Simulation.INSTANCE.getNodeList().get(0).getAdjacent());
+        System.out.println(Simulation.INSTANCE.getNodeList().get(1).getAdjacent());
+        //System.out.println(sim.getNodeList().get(2).getAdjacent());
+        //System.out.println(sim.getVehicles().get(0).getStartNode());
+        //System.out.println(sim.getVehicles().get(0).getEndNode());
     }
 }
