c2cfca69864afb218819993d501ad6f17fa851ba
Adam O Mahony <omahonyadam@gmail.com>
2019-11-17 18:04:23 +0000

    Comment out A_Star until issues fixed to allow maven to build

--- src/main/java/CS4125/TrafficControl/ITCM.java
+++ src/main/java/CS4125/TrafficControl/ITCM.java
@@ -1,4 +1,14 @@
 package CS4125.TrafficControl;
 
-public interface ITCM {
+
+import CS4125.utils.IGraphable;
+
+/**
+ * Interface that all Traffic Control Management classes will implement
+ * Defines methods that all types of TCMs require
+ */
+public interface ITCM extends IGraphable {
+    // TODO: 17-11-19 See if saving all possible states as an enum here is possible/ worth while
+
+    abstract void updateState();
 }
