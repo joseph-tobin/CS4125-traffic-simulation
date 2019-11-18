6334674edc112e007a43126bf3be6bc841695d27
Adam O Mahony <omahonyadam@gmail.com>
2019-11-18 11:13:15 +0000

    Remove Node class and refactor to Decorator Pattern with SimpleJunction being base concrete class

--- src/main/java/CS4125/TrafficControl/TCMDecorator.java
+++ src/main/java/CS4125/TrafficControl/TCMDecorator.java
@@ -1,4 +1,18 @@
 package CS4125.TrafficControl;
 
-public class TCMDecorator {
+
+/**
+ * Base class of all TCM decorator classes,
+ * Must be declared abstract as it does not implement any methods defined in ITCM.
+ */
+public abstract class TCMDecorator implements ITCM {
+    private ITCM tcm;
+
+    TCMDecorator(ITCM tcm) {
+        this.tcm = tcm;
+    }
+
+    public ITCM getTcm() {
+        return this.tcm;
+    }
 }
