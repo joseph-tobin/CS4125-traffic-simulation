1a0d8db83fe9326a2131cc3c87ea119ebfc587d4
Adam O Mahony <omahonyadam@gmail.com>
2019-11-19 15:00:12 +0000

    add SimpleJunctionTest case

---
+++ src/test/java/CS4125/TrafficControl/SimpleJunctionTest.java
@@ -0,0 +1,90 @@
+package CS4125.TrafficControl;
+
+import CS4125.utils.IGraphable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SimpleJunctionTest {
+
+    private SimpleJunction simpleJunctionUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        simpleJunctionUnderTest = new SimpleJunction(0, 0, new ArrayList<>(Arrays.asList()));
+    }
+
+    @Test
+    void testGetAdjacent() {
+        // Setup
+        final List<ITCM> expectedResult = Arrays.asList();
+
+        // Run the test
+        final List<ITCM> result = simpleJunctionUnderTest.getAdjacent();
+
+        // Verify the results
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void testSetAdjacent() {
+        // Setup
+        final List<ITCM> adj = Arrays.asList();
+
+        // Run the test
+        simpleJunctionUnderTest.setAdjacent(adj);
+
+        // Verify the results
+    }
+
+    @Test
+    void testUpdateState() {
+        // Setup
+
+        // Run the test
+        simpleJunctionUnderTest.updateState();
+
+        // Verify the results
+    }
+
+    @Test
+    void testGetHeuristic() {
+        // Setup
+        final int expectedResult = 0;
+
+        // Run the test
+        final int result = simpleJunctionUnderTest.getHeuristic();
+
+        // Verify the results
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void testGetPossibleNext() {
+        // Setup
+        final List<IGraphable> expectedResult = Arrays.asList();
+
+        // Run the test
+        final List<IGraphable> result = simpleJunctionUnderTest.getPossibleNext();
+
+        // Verify the results
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void testCompareTo() {
+        // Setup
+        final int expectedResult = 0;
+
+        // Run the test
+        final int result = simpleJunctionUnderTest.compareTo("o");
+
+        // Verify the results
+        assertEquals(expectedResult, result);
+    }
+}
