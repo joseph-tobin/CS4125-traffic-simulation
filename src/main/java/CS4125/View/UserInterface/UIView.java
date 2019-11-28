590813057ccd816e17959bc1a4c5fa9f73f76754
nialldillane <niall.dillane@gmail.com>
2019-11-28 18:56:50 +0000

    UI revamp

--- src/main/java/CS4125/View/UserInterface/UIView.java
+++ src/main/java/CS4125/View/UserInterface/UIView.java
@@ -1,35 +1,36 @@
 package CS4125.View.UserInterface;
 
+//import CS4125.Controller.Sim.Simulation;
 import CS4125.Controller.Sim.Simulation;
 import CS4125.View.EventHandlers.UIController;
+import CS4125.View.UISim;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.SplitPane;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.shape.*;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class UIView extends Application {
 
 	private SplitPane split;
-	private Pane pane;
+	private Pane simPane;
 	private Scene scene;
 	private VBox controls;
 	private Slider slider;
 	private GridPane tcms;
 	private BorderPane root;
 	private HBox ioButtons;
-	private Button saveBtn;
 
 	private UIController controller;
-	private UIController.ClickHandler clickController;
-	private Simulation sim;
+	private Stage stage;
+//	private UIController.ClickHandler clickController;
 
 
 	/**
@@ -47,8 +48,7 @@ public class UIView extends Application {
 	public void start(Stage stage) throws Exception {
 		split = new SplitPane();
 		split.setDividerPositions(0.75);
-		pane = new Pane(new Label("Sim"));
-
+		simPane = new Pane(new Label("Sim"));
 		controls = new VBox();
 		controls.setSpacing(10);
 
@@ -66,6 +66,7 @@ public class UIView extends Application {
 		slider = new Slider(0, 100, 20);
 		slider.setShowTickMarks(true);
 
+		this.stage = stage;
 		setup(stage);
 	}
 
@@ -77,6 +78,10 @@ public class UIView extends Application {
 		controls.getChildren().add(new Label("Controls"));
 
 		controls.getChildren().add(new Label("Traffic Level:"));
+		slider.valueProperty().addListener(event-> {
+			System.out.println(slider.getValue());
+			// reduce interval for createVehicle thread
+		});
 		controls.getChildren().add(slider);
 
 		controls.getChildren().add(new Label("TCMs:"));
@@ -84,10 +89,13 @@ public class UIView extends Application {
 			double r = 15;
 			Button btn = new Button();
 			switch(i) {
-				case 0: btn.setText("S"); break;
-				case 1: btn.setText("T"); break;
-				case 2: btn.setText("R"); break;
+				case 0: btn.setText("S"); btn.setTooltip(new Tooltip("Simple Junction")); break;
+				case 1: btn.setText("T"); btn.setTooltip(new Tooltip("Traffic Lights")); break;
+				case 2: btn.setText("R"); btn.setTooltip(new Tooltip("Roundabout")); break;
 			}
+			btn.setOnAction(event -> {
+				addTCMPane(btn.getTooltip().getText(), "A"); // prompt user to enter co-ords and nodes connected to it
+			});
 			btn.setShape(new Circle(r));
 			btn.setMinSize(2 * r, 2 * r);
 			btn.setMaxSize(2 * r, 2 * r);
@@ -97,25 +105,85 @@ public class UIView extends Application {
 		tcms.getStyleClass().add("button-container");
 		controls.getChildren().add(tcms);
 
-		saveBtn = new Button("Save");
-		ioButtons.getChildren().add(saveBtn);
+		Button saveBtn = new Button("Save");
+		saveBtn.setOnAction(event -> {
+			// save setup to database
+		});
+
+		Button loadBtn = new Button("Load");
+		loadBtn.setOnAction(event -> {
+			// load setup from database
+			// *** run sim with those metrics
+		});
+
+		Button refreshBtn = new Button("Refresh");
+		refreshBtn.setOnAction(event -> {
+			// reset to default sim
+			Simulation.INSTANCE.run();
+		});
+
+		Button playPauseBtn = new Button("Play");
+		playPauseBtn.setOnAction(event -> {
+			// if play, set to pause and stop animations
+			// if pause, set to play and restart animations
+		});
+
+		ioButtons.getChildren().addAll(playPauseBtn, refreshBtn, saveBtn, loadBtn);
 		ioButtons.setAlignment(Pos.CENTER_RIGHT);
 
-		split.getItems().addAll(pane, root);
+		split.getItems().addAll(simPane, root);
 		scene = new Scene(split, 900, 600);
 		stage.setScene(scene);
 		stage.show();
 
 		controller = new UIController(this);
-		clickController = controller.new ClickHandler(new Circle(-1));
-		pane.addEventHandler(MouseEvent.ANY,
-				new UIController(this).new ClickHandler(new Circle(-1)));
 
 		Simulation.INSTANCE.init(controller);
-//		Simulation.INSTANCE.run();
+		Simulation.INSTANCE.run();
 	}
 
-	public Pane getPane() {
-		return pane;
+	/**
+	 * For adding a TCM to the UI, via clicking on one of the menu items
+	 * @param tcmType
+	 * @param name
+	 */
+	private void addTCMPane(String tcmType, String name) {
+		final Stage dialog = new Stage();
+		dialog.initModality(Modality.APPLICATION_MODAL);
+		dialog.initOwner(stage);
+		dialog.setTitle("Add a " + tcmType);
+		VBox dialogVbox = new VBox(20);
+
+		Label coordTitle = new Label("Co-ordinates:");
+		TextField x_input = new TextField();
+		x_input.setPromptText("x co-ord");
+		TextField y_input = new TextField();
+		y_input.setPromptText("y co-ord");
+		Button coordsBtn = new Button("Save");
+		coordsBtn.setOnAction(event -> {
+			System.out.print(x_input.getText() + "; " + y_input.getText());
+			// grab x & y, add node
+			 Simulation.INSTANCE.addNode(
+			 		tcmType, name, Integer.parseInt(x_input.getText()), Integer.parseInt(y_input.getText()), 1000);
+		});
+
+		Label nodeTitle = new Label("Connect to a node:");
+		TextField node_input = new TextField("node");
+		Button nodeBtn = new Button("Add");
+		nodeBtn.setOnAction(event -> {
+			System.out.println(node_input.getText());
+			node_input.clear();
+//			sim.addEdge();
+		});
+
+		dialogVbox.getChildren().addAll(coordTitle, x_input, y_input, coordsBtn, nodeTitle, node_input, nodeBtn);
+
+		Scene dialogScene = new Scene(dialogVbox, 300, 300);
+		dialog.setScene(dialogScene);
+		dialog.show();
+	}
+
+	public Pane getSimPane() {
+		return simPane;
 	}
 }
