ce63da33b9710a4f46cc130929659282cf3e4f78
Adam <omahonyadam@gmail.com>
2019-11-29 11:17:10 +0000

    Implement boilerplate tests for A_Star and SimpleJunction

--- src/test/java/CS4125/Model/TrafficControl/SimpleJunctionTest.java
+++ src/test/java/CS4125/Model/TrafficControl/SimpleJunctionTest.java
@@ -8,21 +8,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleJunctionTest {
 
     private SimpleJunction simpleJunctionUnderTest;
+    private SimpleJunction simpleJunctionUnderTestAdj1;
+    private SimpleJunction simpleJunctionUnderTestAdj2;
 
     @BeforeEach
     void setUp() {
-        simpleJunctionUnderTest = new SimpleJunction(0, 0, new ArrayList<>(Arrays.asList()));
+        simpleJunctionUnderTest = new SimpleJunction(0, 0, null);
+        simpleJunctionUnderTestAdj1 = new SimpleJunction(1, 1, null);
+        simpleJunctionUnderTestAdj2 = new SimpleJunction(2, 2, null);
+        // add adjacent Junctions
+        simpleJunctionUnderTest.setAdjacent(Arrays.asList(simpleJunctionUnderTestAdj1, simpleJunctionUnderTestAdj2));
     }
 
     @Test
     void testGetAdjacent() {
         // Setup
-        final List<ITCM> expectedResult = Arrays.asList();
+        final List<ITCM> expectedResult = Arrays.asList(simpleJunctionUnderTestAdj1, simpleJunctionUnderTestAdj2);
 
         // Run the test
         final List<ITCM> result = simpleJunctionUnderTest.getAdjacent();
@@ -33,19 +39,23 @@ class SimpleJunctionTest {
 
     @Test
     void testSetAdjacent() {
+        SimpleJunction a = new SimpleJunction(9,9,null);
+        SimpleJunction b = new SimpleJunction(9,9,null);
+        SimpleJunction c = new SimpleJunction(9,9,null);
         // Setup
-        final List<ITCM> adj = Arrays.asList();
+        final List<ITCM> adj = Arrays.asList(a,b,c);
 
         // Run the test
         simpleJunctionUnderTest.setAdjacent(adj);
 
         // Verify the results
+        assertEquals(adj, simpleJunctionUnderTest.getAdjacent());
     }
 
     @Test
     void testUpdateState() {
         // Setup
-
+        // TODO: 29/11/2019 write test for Observer pattern
         // Run the test
         simpleJunctionUnderTest.updateState(1);
 
@@ -56,7 +66,7 @@ class SimpleJunctionTest {
     void testGetHeuristic() {
         // Setup
         final int expectedResult = 0;
-
+        // TODO: 29/11/2019 implement better heuristic values and test again
         // Run the test
         final int result = simpleJunctionUnderTest.getHeuristic();
 
@@ -67,7 +77,7 @@ class SimpleJunctionTest {
     @Test
     void testGetPossibleNext() {
         // Setup
-        final List<IGraphable> expectedResult = Arrays.asList();
+        final List<IGraphable> expectedResult = Arrays.asList(simpleJunctionUnderTestAdj1, simpleJunctionUnderTestAdj2);
 
         // Run the test
         final List<IGraphable> result = simpleJunctionUnderTest.getPossibleNext();
@@ -80,9 +90,11 @@ class SimpleJunctionTest {
     void testCompareTo() {
         // Setup
         final int expectedResult = 0;
+        final ITCM sjUnderTestCopy = new SimpleJunction(0, 0, null);
+        sjUnderTestCopy.setAdjacent(Arrays.asList(simpleJunctionUnderTestAdj1, simpleJunctionUnderTestAdj2));
 
         // Run the test
-        final int result = simpleJunctionUnderTest.compareTo("o");
+        final int result = simpleJunctionUnderTest.compareTo(sjUnderTestCopy);
 
         // Verify the results
         assertEquals(expectedResult, result);
--- src/test/java/CS4125/Model/Utils/A_StarTest.java
+++ src/test/java/CS4125/Model/Utils/A_StarTest.java
@@ -2,6 +2,7 @@ package CS4125.Model.Utils;
 
 import CS4125.Model.TrafficControl.ITCM;
 import CS4125.Model.TrafficControl.SimpleJunction;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -12,22 +13,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class A_StarTest {
 
+    private ITCM start, end, B, C, D;
+
+    @BeforeEach
+    void setUp() {
+        start = new SimpleJunction(1, 1, null);    start.setName("start");
+        end = new SimpleJunction(6, 3, null);      end.setName("end");
+        B = new SimpleJunction(5,1,null);          B.setName("B");
+        C = new SimpleJunction(3,4,null);          C.setName("C");
+        D = new SimpleJunction(5,4,null);          D.setName("D");
+    }
+
+    @Test
+    void testFindRouteSimple() {
+        // Setup
+        start.setAdjacent((Arrays.asList(B, C)));
+        B.setAdjacent((Arrays.asList(start,C)));
+        C.setAdjacent((Arrays.asList(start,B,D)));
+        D.setAdjacent((Arrays.asList(C, end)));
+        end.setAdjacent((Arrays.asList(D)));
+
+        final List<IGraphable> expectedResult = Arrays.asList(start, C, D, end);
+
+        // Run the test
+        final List<IGraphable> result = A_Star.findRoute(start, end);
+
+        // Verify the results
+        assertEquals(expectedResult, result);
+    }
+
     @Test
     void testFindRoute() {
         // Setup
-        ITCM start = new SimpleJunction(1, 1, null);
-        ITCM end = new SimpleJunction(6, 3, null);
-        ITCM B = new SimpleJunction(5,1,null);
-        ITCM C = new SimpleJunction(3,4,null);
-        ITCM D = new SimpleJunction(5,4,null);
+        start.setAdjacent((Arrays.asList(B)));
+        B.setAdjacent((Arrays.asList(start, end)));
+        end.setAdjacent((Arrays.asList(B)));
 
-        start.setAdjacent((Arrays.asList(B, C)));
-        end.setAdjacent((Arrays.asList(D)));
-        B.setAdjacent((Arrays.asList(start, C)));
-        C.setAdjacent((Arrays.asList(start, B)));
-        D.setAdjacent((Arrays.asList(end, C)));
-
-        final List<IGraphable> expectedResult = Arrays.asList(end, D, C, start);
+        final List<IGraphable> expectedResult = Arrays.asList(start, B, end);
 
         // Run the test
         final List<IGraphable> result = A_Star.findRoute(start, end);
