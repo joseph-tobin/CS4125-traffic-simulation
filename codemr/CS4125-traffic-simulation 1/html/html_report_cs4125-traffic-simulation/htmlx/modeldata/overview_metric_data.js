8f51f1f158c7f111c5585cde16343d0c673a0e33
JoeTheToe <16163842@studentmail.ul.ie>
2019-12-01 03:14:08 +0000

    removing code mr

--- codemr/CS4125-traffic-simulation 1/html/html_report_cs4125-traffic-simulation/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"CS4125-traffic-simulation working set: {CS4125-traffic-simulation}", "Number of Highly Problematic Classes":0, "Number of Entities":25, "Number of Problematic Classes":3, "Class Lines of Code":666, "Number of External Packages":16, "Number of Packages":9, "Number of External Entities":54}
--- codemr/CS4125-traffic-simulation/html/workingsets.js
+++
@@ -1,3 +0,0 @@
-var EQ_workingSetList = [
-{name: 'CS4125-traffic-simulation', path:'cs4125-traffic-simulation'}
-];
