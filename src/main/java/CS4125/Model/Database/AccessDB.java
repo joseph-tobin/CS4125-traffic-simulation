0325a898d0388ef707c9a5971753067e3050e8a4
Adam <omahonyadam@gmail.com>
2020-03-08 12:25:43 +0000

    comment out broken DB connection class

--- src/main/java/CS4125/Model/Database/AccessDB.java
+++ src/main/java/CS4125/Model/Database/AccessDB.java
@@ -1,39 +1,39 @@
-package CS4125.Model.Database;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.file.Path;
-
-public class AccessDB {
-    FileInputStream serviceAccount;
-    FirebaseOptions options;
-    FirebaseApp app;
-    DatabaseReference ref;
-    String id;
-    public AccessDB() {
-        try {
-            serviceAccount = new FileInputStream("src/main/resources/cs4125-2c28e-firebase-adminsdk-4v6u7-ca9f7a3067.json");
-            options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl("https://cs4125-2c28e.firebaseio.com")
-                    .build();
-            app=FirebaseApp.initializeApp(options);
-            ref=FirebaseDatabase.getInstance().getReference();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public void write(String in) {
-        TestData quote=new TestData(in);
-        id=ref.push().getKey();
-        ref.child(id).setValueAsync(quote);
-    }
-}
+//package CS4125.Model.Database;
+//
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseOptions;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//import java.nio.file.Path;
+//
+//public class AccessDB {
+//    FileInputStream serviceAccount;
+//    FirebaseOptions options;
+//    FirebaseApp app;
+//    DatabaseReference ref;
+//    String id;
+//    public AccessDB() {
+//        try {
+//            serviceAccount = new FileInputStream("src/main/resources/cs4125-2c28e-firebase-adminsdk-4v6u7-ca9f7a3067.json");
+//            options = new FirebaseOptions.Builder()
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    .setDatabaseUrl("https://cs4125-2c28e.firebaseio.com")
+//                    .build();
+//            app=FirebaseApp.initializeApp(options);
+//            ref=FirebaseDatabase.getInstance().getReference();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//    public void write(String in) {
+//        TestData quote=new TestData(in);
+//        id=ref.push().getKey();
+//        ref.child(id).setValueAsync(quote);
+//    }
+//}
