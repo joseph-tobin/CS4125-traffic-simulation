1ec586a06820cf3db2fbf41de8cc3075f983e4b8
JoeTheToe <16163842@studentmail.ul.ie>
2020-04-07 15:14:57 +0100

    added restore memento pane

--- src/main/java/CS4125/View/UserInterface/Command/MementoRestoreCommand.java
+++ src/main/java/CS4125/View/UserInterface/Command/MementoRestoreCommand.java
@@ -3,8 +3,15 @@ package CS4125.View.UserInterface.Command;
 import CS4125.Controller.Sim.Simulation;
 
 public class MementoRestoreCommand implements ICommand{
+
+    private Simulation.Memento mem;
+
+    public MementoRestoreCommand(Simulation.Memento mem) {
+        this.mem = mem;
+    }
     public void execute() {
-        Simulation.INSTANCE.getSavedSims().get(0).restoreFromMemento();
+        //Simulation.INSTANCE.getSavedSims().get(0).restoreFromMemento();
+        mem.restoreFromMemento();
     }
     public void undo() {
         // TODO: Doesn't do anything ,user popup?
